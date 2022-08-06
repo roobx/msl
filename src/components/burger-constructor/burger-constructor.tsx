1f64537b889d6b664a192c6bd9770623bceed52d
Marsel Salimzhanov <ya.tarasell@yandex.ru>
2022-08-06 21:15:23 +0300

    вторая итерация

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,33 +1,75 @@
-import React, { useState } from 'react';
+import React from 'react';
 import burgerConstructorStyles from './burger-constructor.module.css';
 import { ConstructorElement, Button, DragIcon, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { data } from '../../utils/data.js';
 
 function BurgerConstructor() {
 
+  const selectedBunId = "60666c42cc7b410027a1a9b1";
+  const selectedItemsId = [
+    "60666c42cc7b410027a1a9b5",
+    "60666c42cc7b410027a1a9b6",
+    "60666c42cc7b410027a1a9b6",
+    "60666c42cc7b410027a1a9b7",
+    "60666c42cc7b410027a1a9b4",
+    "60666c42cc7b410027a1a9b9"
+  ]
+  const selectedItems = selectedItemsId.map(id => data.find(item => id === item._id));
+
+  console.log(selectedItems);
+
   return (
     <>
+      {
+        data.filter(item => item._id === selectedBunId)
+          .map(i =>
+          (<div key={i._id} className='ml-6'>
+            <ConstructorElement
+              type="top"
+              isLocked={true}
+              text={`${i.name} (верх)`}
+              price={i.price}
+              thumbnail={i.image}
+            />
+          </div>
+          )
+          )
+      }
       <div className={burgerConstructorStyles.items}>
         {
-          data.map(item =>
+          selectedItems.map(item =>
           (
-            <div className={burgerConstructorStyles.item}>
+            item &&
+            <div key={item._id} className={burgerConstructorStyles.item} >
               <DragIcon type="primary" />
-
               <ConstructorElement
-
-                isLocked={true}
+                isLocked={false}
                 text={item.name}
                 price={item.price}
                 thumbnail={item.image}
-
               />
             </div>
           )
-
           )
+
         }
+
       </div>
+      {
+        data.filter(item => item._id === selectedBunId)
+          .map(i =>
+          (<div key={i._id} className='ml-6'>
+            <ConstructorElement
+              type="bottom"
+              isLocked={true}
+              text={`${i.name} (низ)`}
+              price={i.price}
+              thumbnail={i.image}
+            />
+          </div>
+          )
+          )
+      }
       <div className={burgerConstructorStyles.button_container}>
         <div className={burgerConstructorStyles.price}>
           <p className='mt-1 mb-1 mr-2 text text_type_main-large'>610</p>
--- src/components/burger-ingridients/burger-ingridients.tsx
+++ src/components/burger-ingridients/burger-ingridients.tsx
@@ -19,7 +19,7 @@ function BurgerIngridients() {
           Соусы
         </Tab>
         <Tab value="three" active={current === 'three'} onClick={setCurrent}>
-          Напитки
+          Начинки
         </Tab>
       </div>
       <div className={`mt-10 ${burgerIngridientsStyles.items}`}>
@@ -45,6 +45,24 @@ function BurgerIngridients() {
         <div className={`pl-4 pr-4 ${burgerIngridientsStyles.grid_container}`}>
           {
             data.filter(item => item.type === 'sauce')
+              .map((i) => (
+                <div key={i._id} className={burgerIngridientsStyles.grid_element}>
+                  <img src={i.image} />
+                  <Counter count={1} size="default" />
+                  <div className={burgerIngridientsStyles.item_price}>
+                    <p className='mt-1 mb-1 mr-2'>{i.price}</p>
+                    <CurrencyIcon type="primary" />
+                  </div>
+                  <p className={`text text_type_main-default ${burgerIngridientsStyles.item_name}`}>{i.name}</p>
+                </div>
+              )
+              )
+          }
+        </div>
+        <p className='text text_type_main-medium mb-6 mt-10'>Начинки</p>
+        <div className={`pl-4 pr-4 ${burgerIngridientsStyles.grid_container}`}>
+          {
+            data.filter(item => item.type === 'main')
               .map((i) => (
                 <div key={i._id} className={burgerIngridientsStyles.grid_element}>
                   <img src={i.image} />
