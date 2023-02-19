10b303b858f8e6261eea2adadf97bd5728004e3e
ignat.rubis <ignat.rubis@team.bumble.com>
2023-02-19 10:53:14 +0000

    ghpages

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import './index.css';
-import App from './components/app/app';
-import { Provider } from 'react-redux';
-import { rootReducer } from './services/reducers/index';
-import { socketMiddleware } from './services/middleware/socket-middleware';
-import { wsActions } from './utils/types';
-import { createRoot } from 'react-dom/client';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import "./index.css";
+import App from "./components/app/app";
+import { socketMiddleware } from "./services/middleware/socket-middleware";
+import { Provider } from "react-redux";
+import { rootReducer } from "./services/reducers/index";
+import { createRoot } from "react-dom/client";
+import { wsActions } from "./utils/types";
 
 declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -20,13 +20,10 @@ const enhancer = composeEnhancers(applyMiddleware(thunk), applyMiddleware(socket
 
 export const store = createStore(rootReducer, enhancer);
 
-const root = createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
-
